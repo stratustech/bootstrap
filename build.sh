ace0b89e915a30103aff1d5bbe2a404ea6b3dd44
James Powers <james.powers@stratus.com>
2013-04-09 15:42:24 -0400

    debugging make

--- build.sh
+++ build.sh
@@ -57,7 +57,7 @@ npm install
 
 # run the actual UI build
 # make bootstrap-css
-recess ./less/bootstrap.less --compile
+./node_modules/.bin/recess ./less/bootstrap.less --compile
 
 # Just make sure this sucker exists and is in the expected format
 # tar tzf assets.tgz >/dev/null
